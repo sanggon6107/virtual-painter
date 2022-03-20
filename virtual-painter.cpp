d593dc93d9bfa9a6632e7914685f552288c029fe
sanggon6107 <sanggon6107@naver.com>
2022-03-20 15:34:21 +0900

    DrawOnCanvas 기능 구현

--- virtual-painter.cpp
+++ virtual-painter.cpp
@@ -3,18 +3,22 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 using namespace cv;
 
-enum HSV {HMIN = 0, HMAX = 1, SMIN = 2, SMAX = 3, VMIN = 4, VMAX = 5};
+enum class HSV {HMIN = 0, HMAX = 1, SMIN = 2, SMAX = 3, VMIN = 4, VMAX = 5};
+
 Mat img;
+vector<vector<int>> new_points; // 벡터가 아니라 어레이여도 된다.
 
 
 // hmin 17, hmax 24, smin 114, smax 184, vmin 178, vmax 255
 vector<vector<int>> my_colors{ {17, 24, 114, 184, 178, 255}, // yellow
 								{117, 135, 66, 124, 125, 240} }; // purple
-vector<vector<int>> pen_colors{ {255, 255, 0},   // yellow
+vector<Scalar> pen_colors{ {0, 255, 255},   // yellow
 								{255, 0, 255} }; // purple
 
 Point GetContours(Mat mask)
@@ -36,8 +40,8 @@ Point GetContours(Mat mask)
 		if (int area = contourArea(contours[i]);
 						area > 1000			  )
 		{
-			float peri = arcLength(contours[i], true);				   // ?
-			approxPolyDP(contours[i], con_poly[i], 0.02 * peri, true); // ?
+			double peri = arcLength(contours[i], true);
+			approxPolyDP(contours[i], con_poly[i], 0.02 * peri, true); 
 			bound_rect[i] = boundingRect(con_poly[i]);
 			
 			
@@ -55,24 +59,43 @@ Point GetContours(Mat mask)
 }
 
 
-void FindColor(Mat img)
+vector<vector<int>> FindColor(Mat img)
 {
 	Mat imgHSV;
 	Mat mask;
 	cvtColor(img, imgHSV, COLOR_BGR2HSV);
 	
 	
-	for (int i = 0; i < my_colors.size(); i++)
+	for (int color = 0; color < my_colors.size(); color++)
 	{
-		Scalar lower(my_colors[i][HSV::HMIN], my_colors[i][HSV::SMIN], my_colors[i][HSV::VMIN]);
-		Scalar upper(my_colors[i][HSV::HMAX], my_colors[i][HSV::SMAX], my_colors[i][HSV::VMAX]);
+		Scalar lower(my_colors[color][static_cast<int>(HSV::HMIN)],
+					my_colors[color][static_cast<int>(HSV::SMIN)],
+					my_colors[color][static_cast<int>(HSV::VMIN)]);
+
+		Scalar upper(my_colors[color][static_cast<int>(HSV::HMAX)],
+					my_colors[color][static_cast<int>(HSV::SMAX)],
+					my_colors[color][static_cast<int>(HSV::VMAX)]);
 
 		inRange(imgHSV, lower, upper, mask);
-		imshow(to_string(i), mask);
+		imshow(to_string(color), mask);
 		Point drawing_point_on_img = GetContours(mask);
+
+		if (drawing_point_on_img.x != 0 && drawing_point_on_img.y != 0)
+		{
+			new_points.emplace_back( drawing_point_on_img.x, drawing_point_on_img.y, color );
+		}
 	}
-	
+	return new_points;
 }
+
+void DrawOnCanvas(vector<vector<int>> new_points, vector<Scalar> pen_colors)
+{
+	for (int i = 0; i < new_points.size(); i++)
+	{
+		circle(img, Point(new_points[i][0], new_points[i][1]), 10, pen_colors[new_points[i][2]], FILLED);
+	}
+}
+
 
 
 int main()
@@ -83,8 +106,8 @@ int main()
 	while (true)
 	{
 		cap.read(img);
-		FindColor(img);
-
+		new_points = FindColor(img);
+		DrawOnCanvas(new_points, pen_colors);
 		imshow("image", img);
 		if (waitKey(30) >= 0) break;
 	}
